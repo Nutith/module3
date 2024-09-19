d3ea86c843c2d42c2103c60ef90d7b2ee0d57b51
Nutith <dvedenev@hotmail.com>
2024-09-19 13:02:39 +0300

    Внесены дополнительные изменения для корректной обработки числа 0 (ноль)

--- 5.py
+++ 5.py
@@ -3,13 +3,21 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
 
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        multiplier = get_multiplied_digits(int(str_number[1:]))
+        if multiplier:
+            return first * get_multiplied_digits(int(str_number[1:]))
 
     if first != 0:
         return first
 
-    return 1
+    return None
 
 
 result = get_multiplied_digits(40203)
 print(result)
+
+print('Дополнительные тесты:')
+print(get_multiplied_digits(4020))
+print(get_multiplied_digits(0))
+print(get_multiplied_digits(1))
+print(get_multiplied_digits(1111))
