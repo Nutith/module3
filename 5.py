08605d29e610daff9ffd1b6b91bc3c534b63784f
Nutith <dvedenev@hotmail.com>
2024-09-19 17:01:57 +0300

    косметическая оптимизация

--- 5.py
+++ 5.py
@@ -5,7 +5,7 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         multiplier = get_multiplied_digits(int(str_number[1:]))
         if multiplier:
-            return first * get_multiplied_digits(int(str_number[1:]))
+            return first * multiplier
 
     return first
 
